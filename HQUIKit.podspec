aa0a99f8f655171ddd7f54ccb475efbaac30e7be
guohongqi-china <820003039@qq.com>
2020-01-02 20:32:09 +0800

    UI框架

--- HQUIKit.podspec
+++ HQUIKit.podspec
@@ -8,7 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HQUIKit'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3
+  '
   s.summary          = 'iOS UI handle Kit HQUIKit.'
 
 
@@ -25,7 +26,23 @@ TODO: iOS UI handle Kit HQUIKit.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'HQUIKit/Classes/**/*'
+  s.source_files = 'HQUIKit/Classes/HQUIKitHeader.h'
+  s.public_header_files = 'HQUIKit/Classes/HQUIKitHeader.h'
+  
+  pch_AF = <<-EOS
+#ifndef TARGET_OS_IOS
+  #define TARGET_OS_IOS TARGET_OS_IPHONE
+#endif
+
+EOS
+    s.prefix_header_contents = pch_AF
+    s.ios.deployment_target = '8.0'
+
+    s.subspec 'UIFramework' do |ss|
+      ss.source_files = 'HQUIKit/Classes/UIFramework/**/*.{h,m}'
+      ss.public_header_files = 'HQUIKit/Classes/UIFramework/**/*.{h}'
+      ss.ios.frameworks = 'MobileCoreServices', 'CoreGraphics'
+    end
 
 
 end
---
+++ HQUIKit/Classes/UIFramework/HQQRCodeTools.m
@@ -0,0 +1,168 @@
+//
+//  HQQRCodeTools.m
+//  ZTDJNewProj
+//
+//  Created by guohongqi on 2020/1/2.
+//  Copyright © 2020 guohongqi. All rights reserved.
+//
+
+#import "HQQRCodeTools.h"
+#import <CoreImage/CoreImage.h>
+
+@implementation HQQRCodeTools
+
+#pragma mark --  interface Methods
+//生成二维码
++ (UIImage *)drawQRCodeWithContent:(NSString *)content imgSize:(CGFloat)size
+{
+    //创建过滤器
+    CIFilter *filter   = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    //过滤器恢复默认
+    [filter setDefaults];
+    //给过滤器添加数据<字符串长度893>
+    NSData *data       = [content dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    [filter setValue:data forKey:@"inputMessage"];
+    //获取二维码过滤器生成二维码
+    CIImage *image     = [filter outputImage];
+    UIImage *img       = [self createNonInterpolatedUIImageFromCIImage:image WithSize:size];
+    return img;
+}
+
++ (UIImage *)drawQRCodeWithContent:(NSString *)content imgSize:(CGFloat)size withLogoImg:(UIImage *)logoImg{
+    UIImage *qrImg     = [HQQRCodeTools drawQRCodeWithContent:content imgSize:size];
+    UIImage *qrLogoImg = [HQQRCodeTools combinateCodeImage:qrImg andLogo:logoImg];
+    return qrLogoImg;
+}
+
+
+#pragma mark --  private Methods
+//二维码清晰
++ (UIImage *)createNonInterpolatedUIImageFromCIImage:(CIImage *)image WithSize:(CGFloat)size
+{
+    CGRect extent = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    
+    //创建bitmap
+    size_t width = CGRectGetWidth(extent)*scale;
+    size_t height = CGRectGetHeight(extent)*scale;
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+    
+    //保存图片
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [UIImage imageWithCGImage:scaledImage];
+}
+
+// 合成图片（code+logo）
+ + (UIImage *)combinateCodeImage:(UIImage *)codeImage andLogo:(UIImage *)logo {
+    
+    UIGraphicsBeginImageContextWithOptions(codeImage.size, YES, [UIScreen mainScreen].scale);
+    
+    // 将codeImage画到上下文中
+    [codeImage drawInRect:(CGRect){.0, .0, codeImage.size.width, codeImage.size.height}];
+    
+    // 定义logo的绘制参数
+    CGFloat logoSide = fminf(codeImage.size.width, codeImage.size.height) / 4;
+    CGFloat logoX = (codeImage.size.width - logoSide) / 2;
+    CGFloat logoY = (codeImage.size.height - logoSide) / 2;
+    CGRect logoRect = (CGRect){logoX, logoY, logoSide, logoSide};
+    UIBezierPath *cornerPath = [UIBezierPath bezierPathWithRoundedRect:logoRect cornerRadius:logoSide / 5];
+    [cornerPath setLineWidth:2.0];
+    [[UIColor whiteColor] set];
+    [cornerPath stroke];
+    [cornerPath addClip];
+    
+    // 将logo画到上下文中
+    [logo drawInRect:logoRect];
+    
+    // 从上下文中读取image
+    codeImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return codeImage;
+}
+
+#pragma mark - 生成条形码
++ (UIImage *)drawBarcodeImageWithContent:(NSString *)content codeImageSize:(CGSize)size red:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue{
+    UIImage *image = [self barcodeImageWithContent:content codeImageSize:size];
+    int imageWidth = image.size.width;
+    int imageHeight = image.size.height;
+    size_t bytesPerRow = imageWidth * 4;
+    uint32_t *rgbImageBuf = (uint32_t *)malloc(bytesPerRow * imageHeight);
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(rgbImageBuf, imageWidth, imageHeight, 8, bytesPerRow, colorSpaceRef, kCGBitmapByteOrder32Little|kCGImageAlphaNoneSkipLast);
+    CGContextDrawImage(context, CGRectMake(0, 0, imageWidth, imageHeight), image.CGImage);
+    //遍历像素, 改变像素点颜色
+    int pixelNum = imageWidth * imageHeight;
+    uint32_t *pCurPtr = rgbImageBuf;
+    for (int i = 0; i<pixelNum; i++, pCurPtr++) {
+        if ((*pCurPtr & 0xFFFFFF00) < 0x99999900) {
+            uint8_t* ptr = (uint8_t*)pCurPtr;
+            ptr[3] = red*255;
+            ptr[2] = green*255;
+            ptr[1] = blue*255;
+        }else{
+            uint8_t* ptr = (uint8_t*)pCurPtr;
+            ptr[0] = 0;
+        }
+    }
+    //取出图片
+    CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, rgbImageBuf, bytesPerRow * imageHeight, ProviderReleaseData);
+    CGImageRef imageRef = CGImageCreate(imageWidth, imageHeight, 8, 32, bytesPerRow, colorSpaceRef,
+                                        kCGImageAlphaLast | kCGBitmapByteOrder32Little, dataProvider,
+                                        NULL, true, kCGRenderingIntentDefault);
+    CGDataProviderRelease(dataProvider);
+    UIImage *resultImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpaceRef);
+    
+    return resultImage;
+}
+
+//改变条形码尺寸大小
++ (UIImage *)barcodeImageWithContent:(NSString *)content codeImageSize:(CGSize)size{
+    CIImage *image = [self barcodeImageWithContent:content];
+    CGRect integralRect = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size.width/CGRectGetWidth(integralRect), size.height/CGRectGetHeight(integralRect));
+    
+    size_t width = CGRectGetWidth(integralRect)*scale;
+    size_t height = CGRectGetHeight(integralRect)*scale;
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, colorSpaceRef, (CGBitmapInfo)kCGImageAlphaNone);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:integralRect];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, integralRect, bitmapImage);
+    
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [UIImage imageWithCGImage:scaledImage];
+}
+
+//生成最原始的条形码
++ (CIImage *)barcodeImageWithContent:(NSString *)content{
+    CIFilter *qrFilter = [CIFilter filterWithName:@"CICode128BarcodeGenerator"];
+    NSData *contentData = [content dataUsingEncoding:NSUTF8StringEncoding];
+    [qrFilter setValue:contentData forKey:@"inputMessage"];
+        [qrFilter setValue:@(0.00) forKey:@"inputQuietSpace"];
+    CIImage *image = qrFilter.outputImage;
+    return image;
+}
+
+void ProviderReleaseData (void *info, const void *data, size_t size){
+    free((void*)data);
+}
+
+
+@end
